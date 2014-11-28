2e24f1126b7a659798d026c2db64a36d9017cda0
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2014-11-28 09:36:31 -0500

    added return to top button

---
+++ js/returntop.js
@@ -0,0 +1,23 @@
+var speed = 100;
+var fade = 500;
+
+$(document).ready(function(){
+    $(window).bind('scroll', function(){
+        if ($(this).scrollTop() > 150) {
+            $('.rtt').fadeIn(fade);
+        } else {
+            $('.rtt').fadeOut(fade);
+        }
+    });
+});
+
+$(function () {
+    $('body').on("click", ".rtt", function (e) {
+
+        e.preventDefault();
+
+        $('body,html').animate({
+            scrollTop: speed
+        }, speed);
+    });
+});
